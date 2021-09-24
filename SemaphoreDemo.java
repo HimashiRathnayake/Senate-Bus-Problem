f32d8a42714d4e8840497ec89915c59ecf657a58
Himashi <nikeshalarathnayake19@gmail.com>
2021-09-24 17:08:00 +0530

    add main class

---
+++ SemaphoreDemo.java
@@ -0,0 +1,15 @@
+public class SemaphoreDemo {
+    public static int riders;
+    public static void main(String args[]) throws InterruptedException {
+        riders = 0;
+
+        float bus_inter_arrival_time = 20 * 60 * 1000;
+        float riders_inter_arrival_time = 30 * 1000;
+
+        RidersGenerator riderGenerator = new RidersGenerator(riders_inter_arrival_time);
+        riderGenerator.start();
+
+        BusGenerator busGenerator = new BusGenerator(bus_inter_arrival_time);
+        busGenerator.start();
+    }
+}
